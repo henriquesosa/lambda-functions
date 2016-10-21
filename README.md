4acc0ee63aeabb80374546e4e4e7683ec4ac5f7b
Henrique Sosa <henriquesosa@gmail.com>
2016-10-20 22:56:40 -0200

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lamba Functions
+
+I created this repo to make some examples of how to build AWS Lambda functions using in Python
+
+## Examples
+
+[Exchange rates](examples/exchange.py)
---
+++ examples/exchange.py
@@ -0,0 +1,35 @@
+import urllib
+import json
+import boto3
+
+def lambda_handler(event, context):
+
+    request = "API URL HERE"
+    response = json.loads(request)
+
+    # AWS SES client
+    ses.boto3.client('ses')
+
+    email_from = "johndoe@email.com"
+    email_to = "johndoe2@email.com"
+    subject = "Exchange rate"
+    body = "Dollar: U${0}". format(response['field'])
+
+    return ses.send_email(
+        Source = email_from,
+        Destination = {
+            "ToAddresses" : [email_to]
+        },
+        Message = {
+            "Subject": {
+                "Charset" : "UTF-8",
+                "Data" : subject
+            },
+            "Body": {
+                "Html" : {
+                    "Charset" : "UTF-8",
+                    "Data" : body
+                }
+            }
+        }
+    )
