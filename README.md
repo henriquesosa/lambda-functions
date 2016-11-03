2daad842d8c7794078fb42969b1ae6e5d52e849b
Henrique Sosa <henriquesosa@gmail.com>
2016-11-03 21:59:56 -0200

    Add Cellular automata example

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ I created this repo to study Python
 ## Examples
 
 [AWS Lambda Exchange Function](examples/aws-lamba-exchange-function.py)
+[Cellular Automata](examples/cellular-automata.py)
---
+++ examples/cellular-automata.py
@@ -0,0 +1,69 @@
+from random import randint
+
+parent_cells = []
+parent_cells_length = 10
+cellss = []
+
+generations = 300
+
+# Apply Wolfram's Rule 110
+def rule_110(cell, left, right):
+    if(left == 1 and cell == 1 and right == 1):
+        return 0
+    elif(left == 1 and cell == 0 and right == 1):
+        return 1
+    elif(left == 0 and cell == 0 and right == 1):
+        return 1
+    else:
+        return cell
+
+# Get the next generation of cells
+def get_next_gerenation(cells):
+    temp_cells = []
+
+    for x in range(0, len(cells)):
+        left = cells[x -1]
+
+        if(x < (len(cells) - 1)):
+            right = cells[x + 1]
+        else:
+            right = cells[x]
+
+        temp_cells.append(rule_110(cells[x], left, right))
+
+    return temp_cells
+
+# Set Initial Random Numbers
+def set_random():
+    temp_cells = []
+
+    for x in range(0, parent_cells_length):
+        temp_cells.append(randint(0,1))
+
+    return temp_cells
+
+#Print line in a beautiful way
+def print_line(cells):
+
+    line = ""
+
+    for cell in cells:
+        if cell == 1:
+            line += " # "
+        else:
+            line += "   "
+
+    print line
+
+
+# Let the game begin
+def begin():
+    parent_cells = set_random()
+
+    for g in range(0, generations):
+        cells = get_next_gerenation(parent_cells)
+        print_line(cells)
+        parent_cells = cells
+
+# Begin
+begin()
