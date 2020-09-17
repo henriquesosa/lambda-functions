7c2ad7f2c69781ff0c4e78eb0923f9ccc8169ea7
Henrique Sosa <henriquesosa@gmail.com>
2020-09-17 04:51:01 -0300

    Reindent files

--- examples/aws-lamba-exchange-function.py
+++ examples/aws-lamba-exchange-function.py
@@ -3,33 +3,31 @@ import json
 import boto3
 
 def lambda_handler(event, context):
+  request = "API URL HERE"
+  response = json.loads(request)
 
-    request = "API URL HERE"
-    response = json.loads(request)
+  ses.boto3.client('ses')
 
-    # AWS SES client
-    ses.boto3.client('ses')
+  email_from = "johndoe@email.com"
+  email_to = "johndoe2@email.com"
+  subject = "Exchange rate"
+  body = "Dollar: U${0}". format(response['field'])
 
-    email_from = "johndoe@email.com"
-    email_to = "johndoe2@email.com"
-    subject = "Exchange rate"
-    body = "Dollar: U${0}". format(response['field'])
-
-    return ses.send_email(
-        Source = email_from,
-        Destination = {
-            "ToAddresses" : [email_to]
-        },
-        Message = {
-            "Subject": {
-                "Charset" : "UTF-8",
-                "Data" : subject
-            },
-            "Body": {
-                "Html" : {
-                    "Charset" : "UTF-8",
-                    "Data" : body
-                }
-            }
+  return ses.send_email(
+    Source = email_from,
+    Destination = {
+      "ToAddresses" : [email_to]
+    },
+    Message = {
+      "Subject": {
+        "Charset" : "UTF-8",
+        "Data" : subject
+      },
+      "Body": {
+        "Html" : {
+          "Charset" : "UTF-8",
+          "Data" : body
         }
-    )
+      }
+    }
+  )
--- examples/cellular-automata.py
+++ examples/cellular-automata.py
@@ -1,90 +1,86 @@
 from random import randint
 
 class Automata:
-    def __init__(self, children, generations):
-        self.parent_cells_length =  children
-        self.generations = generations
-    
-    # Return a random binary
-    def __random_binary(self):
-        return randint(0,1)
+  def __init__(self, children, generations):
+      self.parent_cells_length =  children
+      self.generations = generations
 
-    # Wolfram's Rule 110
-    def __rule_110(self, current, left, right):
-        if(left == 1 and current == 1 and right == 1):
-            return 0
-        elif(left == 1 and current == 1 and right == 0):
-            return 1
-        elif(left == 1 and current == 0 and right == 1):
-            return 1
-        elif(left == 1 and current == 0 and right == 0):
-            return 0
-        elif(left == 0 and current == 1 and right == 1):
-            return 1
-        elif(left == 0 and current == 1 and right == 0):
-            return 1
-        elif(left == 0 and current == 0 and right == 1):
-            return 1
-        elif(left == 0 and current == 0 and right == 0):
-            return 0
-        else:
-            return current
+  # Return a random binary
+  def __random_binary(self):
+      return randint(0,1)
 
-    # Rule switcher
-    def __apply_rule(self, current, left, right, rule):
+  # Wolfram's Rule 110
+  def __rule_110(self, current, left, right):
+      if(left == 1 and current == 1 and right == 1):
+          return 0
+      elif(left == 1 and current == 1 and right == 0):
+          return 1
+      elif(left == 1 and current == 0 and right == 1):
+          return 1
+      elif(left == 1 and current == 0 and right == 0):
+          return 0
+      elif(left == 0 and current == 1 and right == 1):
+          return 1
+      elif(left == 0 and current == 1 and right == 0):
+          return 1
+      elif(left == 0 and current == 0 and right == 1):
+          return 1
+      elif(left == 0 and current == 0 and right == 0):
+          return 0
+      else:
+          return current
 
-        if rule == "110":
-            return self.__rule_110(current, left, right)
-        else:
-            return current
-    
-    def __next_generation(self, old_generation, rule):
-        new_generation = []
+  # Rule switcher
+  def __apply_rule(self, current, left, right, rule):
 
-        for x in range(0, len(old_generation)):
-            left_neighbor = old_generation[x - 1]
-            
-            if(x < (len(old_generation) -1 )):
-                right_neighbor = old_generation[x + 1]
-            else:
-                right_neighbor = old_generation[x]
-            
-            new_generation.append(self.__apply_rule(old_generation[x], left_neighbor, right_neighbor, rule))
-        
-        return new_generation
-    
-    # Return the final array of generations
-    def calc(self, rule):
-        self.parent_cells = []
-        self.cells = []
-        self.final_cells = []
-        
-        for x in range(0, self.parent_cells_length):
-            self.parent_cells.append(self.__random_binary())
-        
-        for y in range(0, self.generations):
-            self.cells = self.__next_generation(self.parent_cells, rule)
-            self.final_cells.append(self.cells)
-            self.parent_cells = self.cells
-        
-        return self.final_cells
+      if rule == "110":
+          return self.__rule_110(current, left, right)
+      else:
+          return current
 
-    #Print line in a beautiful way
-    def print_line(self, lines):
-        
+  def __next_generation(self, old_generation, rule):
+      new_generation = []
 
-        for x in lines:
+      for x in range(0, len(old_generation)):
+          left_neighbor = old_generation[x - 1]
 
-            line = ""
+          if(x < (len(old_generation) -1 )):
+              right_neighbor = old_generation[x + 1]
+          else:
+              right_neighbor = old_generation[x]
 
-            for y in x:
-                if y == 1:
-                    line += " # "
-                else:
-                    line += "   "
+          new_generation.append(self.__apply_rule(old_generation[x], left_neighbor, right_neighbor, rule))
 
-            print line
-            
+      return new_generation
+
+  # Return the final array of generations
+  def calc(self, rule):
+      self.parent_cells = []
+      self.cells = []
+      self.final_cells = []
+
+      for x in range(0, self.parent_cells_length):
+          self.parent_cells.append(self.__random_binary())
+
+      for y in range(0, self.generations):
+          self.cells = self.__next_generation(self.parent_cells, rule)
+          self.final_cells.append(self.cells)
+          self.parent_cells = self.cells
+
+      return self.final_cells
+
+  #Print line in a beautiful way
+  def print_line(self, lines):
+      for x in lines:
+          line = ""
+          for y in x:
+              if y == 1:
+                  line += " # "
+              else:
+                  line += "   "
+
+          print(line)
+
 automata = Automata(71, 300)
 automata_result = automata.calc("110")
 automata.print_line(automata_result)
